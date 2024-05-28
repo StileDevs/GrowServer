2c6074d02a35532b742c9e859ec51c6cc760a4c1
Hendra Gunawan <48410066+JadlionHD@users.noreply.github.com>
2024-05-28 21:51:49 +0700

    feat(command): add Sb.ts
    Former-commit-id: 35333d91f9fd1d9a714af781918e7f5d9888085f

---
+++ src/commands/Sb.ts
@@ -0,0 +1,38 @@
+import { TextPacket, Variant } from "growtopia.js";
+import { Command } from "../abstracts/Command";
+import { BaseServer } from "../structures/BaseServer";
+import { Peer } from "../structures/Peer";
+import { CommandOptions } from "../types/command";
+import { Role } from "../utils/Constants";
+import { DataTypes } from "../utils/enums/DataTypes";
+
+export default class extends Command {
+  public opt: CommandOptions;
+
+  constructor(base: BaseServer) {
+    super(base);
+    this.opt = {
+      name: "sb",
+      description: "Broadcast a message to everyone",
+      cooldown: 5,
+      ratelimit: 1,
+      category: "Basic",
+      usage: "/sb <message>",
+      example: ["/sb hello"],
+      permission: [Role.BASIC, Role.SUPPORTER, Role.DEVELOPER]
+    };
+  }
+
+  public async execute(peer: Peer, text: string, args: string[]): Promise<void> {
+    if (!args.length) return peer.send(Variant.from("Message are required."));
+
+    const world = peer.hasWorld(peer.data.world);
+
+    peer.everyPeer((p) => {
+      p.send(
+        Variant.from("OnConsoleMessage", `CP:0_PL:4_OID:_CT:[SB]_ \`5**\`\` from \`$\`2${peer.name} \`\`\`\`(in \`$${world?.data.jammers?.find((v) => v.type === "signal")?.enabled ? "`4JAMMED``" : peer.data.world}\`\`) ** :\`\` \`#${args.join(" ")}`),
+        TextPacket.from(DataTypes.ACTION, "action|play_sfx", `file|audio/beep.wav`, `delayMS|0`)
+      );
+    });
+  }
+}
