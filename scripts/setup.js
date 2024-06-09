8221b22e660872d15970ba4dc05452094f3e4c86
Kukuri <83948007+Mikasuru@users.noreply.github.com>
2024-06-09 20:47:26 +0700

    Update setup.js for Discord.js

--- scripts/setup.js
+++ scripts/setup.js
@@ -7,6 +7,6 @@ if (!fs.existsSync("./data")) {
 if (!fs.existsSync("./.env")) {
   fs.writeFileSync(
     "./.env",
-    "ENCRYPT_SECRET=SUPERSECRET # Default encrypt secret\nWEB_ADDRESS=127.0.0.1\nWEB_ENV=development\nDEBUG_MODE=false"
+    "ENCRYPT_SECRET=SUPERSECRET # Default encrypt secret\nWEB_ADDRESS=127.0.0.1\nWEB_ENV=development\nDEBUG_MODE=false\nTOKEN=Tokxxxxxxxen\nCLIENTID=123xxxxx\nOWNERID=123xxxxx"
   );
 }
