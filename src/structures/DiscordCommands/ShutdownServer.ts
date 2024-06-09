2b3178e8e37e4de9ef36fb3ce42869a131daca9e
Kukuri <83948007+Mikasuru@users.noreply.github.com>
2024-06-09 20:24:40 +0700

    Update ShutdownServer.ts

--- src/structures/DiscordCommands/ShutdownServer.ts
+++ src/structures/DiscordCommands/ShutdownServer.ts
@@ -5,7 +5,7 @@ import { Logger } from "../Logger";
 
 export default async function ShutdownServer(server: BaseServer, args: string[], msg: Message) {
   const log = new Logger;
-  if(msg.author.id !== "591837095954350092") return msg.reply("Wait, Why would you do this?");
+  if(msg.author.id !== process.env.OWNERID) return msg.reply("Wait, Why would you do this?");
 
   log.action(`>> ${msg.author.username} << - executed the Shutdown command for discord bot`);
   handleSaveAll(server, true);
