2210d1c7096189e25e1c8919ec105a0cb216fc5b
Hendra Gunawan <48410066+JadlionHD@users.noreply.github.com>
2024-09-23 22:04:05 +0700

    rework everything into plugin/module base

--- src/types/action.d.ts
+++
@@ -1,9 +0,0 @@
-export interface ActionConfig {
-  eventName?: string;
-}
-
-// TODO: when there's a something on generic type, then add it to property type.
-export interface ActionDefault {
-  action: string;
-}
-export type ActionType<T> = T;
--- src/utils/enums/DataTypes.ts
+++
@@ -1,6 +0,0 @@
-export enum DataTypes {
-  HELLO = 1,
-  STR = 2,
-  ACTION = 3,
-  TANK = 4
-}
