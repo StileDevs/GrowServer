e890bd376369755d4c8dd3f7e4157fbc8db25909
Hendra Gunawan <48410066+JadlionHD@users.noreply.github.com>
2024-10-22 00:42:56 +0700

    feat: send peer to main menu
    Former-commit-id: 25f67a06be3b9fe4673c1bd067a94c9294d9d78a

---
+++ src/network/actions/EnterGame.ts
@@ -0,0 +1,41 @@
+import { Variant, TankPacket } from "growtopia.js";
+import { Base } from "../../core/Base.js";
+import { Peer } from "../../core/Peer.js";
+import { DialogBuilder } from "../../utils/builders/DialogBuilder.js";
+
+export class EnterGame {
+  constructor(public base: Base) {}
+
+  public async execute(peer: Peer, action: Record<string, { action: string }>): Promise<void> {
+    const tes = new DialogBuilder().defaultColor().addLabelWithIcon("`wThe GrowServer Gazette``", "5016", "big").addSpacer("small").raw("add_image_button||interface/large/banner-transparent.rttex|bannerlayout|||\n").addTextBox("Welcome to GrowServer").addQuickExit().endDialog("gazzette_end", "Cancel", "Ok").str();
+    peer.send(
+      Variant.from(
+        "OnRequestWorldSelectMenu",
+        `
+add_heading|Top Worlds|
+add_floater|START|0|0.5|3529161471
+add_floater|START1|0|0.5|3529161471
+add_floater|START2|0|0.5|3529161471
+${Array.from(this.base.cache.worlds.values())
+  .sort((a, b) => (b.playerCount || 0) - (a.playerCount || 0))
+  .slice(0, 6)
+  .map((v) => {
+    if (v.playerCount) return `add_floater|${v.name}${v.playerCount ? ` (${v.playerCount})` : ""}|0|0.5|3529161471\n`;
+    else return "";
+  })
+  .join("\n")}
+add_heading|Recently Visited Worlds<CR>|
+${peer.data.lastVisitedWorlds
+  ?.reverse()
+  .map((v) => {
+    const count = this.base.cache.worlds.get(v)?.playerCount || 0;
+    return `add_floater|${v}${count ? ` (${count})` : ""}|0|0.5|3417414143\n`;
+  })
+  .join("\n")}
+`
+      ),
+      Variant.from("OnConsoleMessage", `Welcome ${peer.data.tankIDName} Where would you like to go?`),
+      Variant.from({ delay: 100 }, "OnDialogRequest", tes)
+    );
+  }
+}
