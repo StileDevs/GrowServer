bb1cfe097d7b37b27107beb757af227d60ad06b6
Hendra Gunawan <48410066+JadlionHD@users.noreply.github.com>
2024-10-22 00:42:56 +0700

    feat: send peer to main menu

--- src/network/actions/index.ts
+++ src/network/actions/index.ts
@@ -1,6 +1,7 @@
 import { EmptyObject, type Class } from "type-fest";
 import { RefreshItemData } from "./RefreshItemData";
 import { Peer } from "../../core/Peer.js";
+import { EnterGame } from "./EnterGame";
 
 const ActionMap: Record<
   string,
@@ -8,7 +9,8 @@ const ActionMap: Record<
     execute: (peer: Peer, action: Record<string, any>) => Promise<void>;
   }>
 > = {
-  ["refresh_item_data"]: RefreshItemData
+  ["refresh_item_data"]: RefreshItemData,
+  ["enter_game"]: EnterGame
 };
 
 export { ActionMap };
