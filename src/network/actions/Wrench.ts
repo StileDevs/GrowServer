a2cd66b91831453bef0ed53f79e607330e907c94
Kukuri and the Harmonic Odyssey <83948007+Mikasuru@users.noreply.github.com>
2024-12-27 23:28:19 +0700

    Added Store and Wrench
    Store with handler
    Wrench show basic info

---
+++ src/network/actions/Wrench.ts
@@ -0,0 +1,29 @@
+import { type NonEmptyObject } from "type-fest";
+import { Base } from "../../core/Base";
+import { Peer } from "../../core/Peer";
+import { DialogBuilder } from "../../utils/builders/DialogBuilder";
+import { Variant } from "growtopia.js";
+
+export class Wrench {
+  constructor(public base: Base, public peer: Peer) {}
+
+  private InfoDialog(): DialogBuilder {
+    // for now will put basic info
+    return new DialogBuilder()
+      .defaultColor()
+      .addLabelWithIcon(`${this.peer.name}\`w's Information (${this.peer.data.id_user})`, 32, "big")
+      .addTextBox(`Player Infomation`)
+      .addSmallText(`Level: ${this.peer.data.level || 1}`)
+      .addSmallText(`Gems: ${this.peer.data.gems || 0}`)
+      .addSmallText(`NetID: ${this.peer.data.netID}`)
+  }
+
+  public async execute(action: NonEmptyObject<{ itemID: string }>): Promise<void> {
+    const dialog = this.InfoDialog()
+      .endDialog("wrench_end", "Cancel", "OK")
+      .addQuickExit()
+      .str();
+
+    this.peer.send(Variant.from("OnDialogRequest", dialog));
+  }
+}
